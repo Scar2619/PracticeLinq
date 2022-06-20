0b8364d77ad3b93034648946d4a0b05dd427acc4
Katelind Phillips <scarlettkate94@gmail.com>
2022-06-20 16:41:19 -0400

    finalized

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gameNames = new List<string> { "The Sims", "Halo", "Call Of Duty",
+            "MarioKart", "Assassin's Creed", "Animal Crossing"};
+
+            var sorted = gameNames.OrderBy(x => x.Length);
+
+            foreach (var item in sorted)
+            {
+                Console.WriteLine($"{item}");
+            }
+            
+
+        }
+    }
+}
+
